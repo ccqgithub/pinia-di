bed0c5a0c71e2279abb147eeda3e3499122a0396
Carl Olsen <unstoppablecarlolsen@gmail.com>
2025-06-28 17:36:15 -0400

    added return type for getProvideArgs function

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,9 @@
 import { InjectionProvide } from './types';
 import Injector from './injector';
 import { injectorKey } from './context';
+import { InjectionKey } from 'vue';
 
-export const getProvideArgs = (providers: InjectionProvide[], name = '') => {
+export const getProvideArgs = (providers: InjectionProvide[], name = ''): [InjectionKey<Injector>, Injector] => {
   const injector = new Injector(providers, { name });
   return [injectorKey, injector];
 };
