193827e484055939bde618d0144b05df93b8c8b0
Carl Olsen <unstoppablecarlolsen@gmail.com>
2025-07-01 17:08:21 -0400

    merged typo branch

--- src/test.ts
+++
@@ -1,9 +0,0 @@
-import { InjectionContext, Injector } from './index';
-import { defineStore } from 'pinia';
-
-export const VehicleStore = (ctx: InjectionContext) => defineStore(ctx.useStoreId('vehicles'), () => {
-    console.log(ctx)
-})
-const i = new Injector([VehicleStore], {name: 'foo'})
-
-console.log(i)
