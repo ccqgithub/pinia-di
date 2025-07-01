55438e6367adb4c3c4e51eceeec7b52a791d9ff1
Carl Olsen <unstoppablecarlolsen@gmail.com>
2025-07-01 13:01:24 -0400

    cleanup

--- tests/helpers/ComponentA.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { useStore } from '../../src';
-import { TestStoreA } from './test-stores';
-
-const store = useStore(TestStoreA);
-</script>
-<template>
-  {{ store.value }}
-</template>
